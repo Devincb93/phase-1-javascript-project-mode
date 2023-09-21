b979556a5448a77e0e372a544737d8ef631be6f5
Devin <devincb93@gmail.com>
2023-09-21 16:24:49 -0700

    added a few more const to the grouping. targeted the users input and assigned it to a const for easier use.

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 const submitButton = document.querySelector("input[type=submit]");
 const rickAndMortyForm = document.getElementById("rickAndMortyForm");
+const rickAndMortySubmit = document.querySelector("input[type=text]");
 
-rickAndMortyForm.addEventListener.apply("submit", (event) => {
+
+rickAndMortyForm.addEventListener("submit", (event) => {
     event.preventDefault();
-    const userInput = event.target.querySelector("input[type=text]");
-    const 
+
+    const submitEvent = event.target.querySelector("input[type=submit]");
+    const inputValue = submitEvent
+    
+    fetch(`https://rickandmortyapi.com/api/character`)
+    .then(resp => resp.json())
+    .then(characters => {
+     
+     
+
+})
+    
+    const input = event.target.querySelector("input[type=text]");
+    const userInput = input.value;
+    console.log(userInput)
 })
